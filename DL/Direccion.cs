b95b45a722245bfad4e7b34e519c9908fbbed73e
AMoralesS25 <adrianamsan25@gmail.com>
2025-05-15 04:53:13 -0600

    Add project files.

---
+++ DL/Direccion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public  class Direccion
+    {
+        public int IdDireccion { get; set; }
+        [RegularExpression(@"^[A-Za-z0-9\s]{1,50}$", ErrorMessage = "La calle solo debe tener letras y números")]
+        public string Calle { get; set; }
+        [RegularExpression(@"^[A-Za-z0-9\s]{1,20}$", ErrorMessage = "El número interior solo debe tener letras y números")]
+        public string NumeroInterior { get; set; }
+        [RegularExpression(@"^[A-Za-z0-9\s]{1,50}$", ErrorMessage = "La calle solo debe tener letras y números")]
+        public string NumeroExterior { get; set; }
+        public DL.Colonia Colonia { get; set; }
+    }
+}
---
+++ DL/Rol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class Rol
+    {
+        [DisplayName("Rol")]
+        [Required(ErrorMessage = "Selecciona un  rol")]
+        public int IdRol { get; set; }
+        public string Nombre { get; set; }
+    }
+}
